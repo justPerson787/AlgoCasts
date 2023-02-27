3e6713d84839219400dd9a00d80cee3b09290824
justPerson787 <n.n.ushakova@gmail.com>
2023-02-27 14:44:15 -0500

    Reverse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,13 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //str = str.split();
+    let newStr = '';
+    for (i = str.length - 1; i>=0; i--) {
+        newStr +=str[i]
+    }
+    return newStr;
+}
 
 module.exports = reverse;
