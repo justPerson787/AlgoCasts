369a8f28274c1f227a1ef7cdd0eaf59be2d56d53
justPerson787 <n.n.ushakova@gmail.com>
2023-03-06 15:22:34 -0500

    Add most used characters solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let output = {};
+    let max = 0;
+    let maxKey = '';
+    for (let char of str) {
+        if (!output[char]) {
+            output[char] = 1;
+        } else {
+            output[char]++;
+        }
+    }
+    for (let char in output) {
+        if (output[char] > max) {
+            max = output[char];
+            maxKey = char;
+        }
+    }
+    return maxKey;
+}
 
 module.exports = maxChar;
